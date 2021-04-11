a68682a47e33559171469ee619434108aaae46f3
Anderson Teala <andersonteala@gmail.com>
2021-04-10 23:39:52 -0300

    Update

--- quasar-pokedex-v2/src/pages/Index.vue
+++ quasar-pokedex-v2/src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page class="flex flex-center">
     <div class="q-pa-md row items-start q-gutter-md">
       <q-card
-      class="my-card"
+      class="my-card bg-"
       v-for="(pokemons, index) in pokemons"
 			:key="++ index"
       >
@@ -12,6 +12,7 @@
         <q-card-section>
           <div class="text-subtitle2">{{ pokemons.name }}</div>
           <div class="text-subtitle2">#{{ index }}</div>
+          <div class="text-subtitle2">Color: {{ species + index }}</div>
         </q-card-section>
       </q-card>
     </div>
@@ -27,22 +28,36 @@ export default {
     return {
       index: [],
       pokemons: [],
+      colorUrl: [],
+      species: 'https://pokeapi.co/api/v2/pokemon-species/',
       pokemonsImages: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/',
       images: [],
     }
   },
   mounted(){
     this.getPokemon()
+    this.getColor()
   },
   methods: {
     getPokemon(){
       this.$axios.get('https://pokeapi.co/api/v2/pokemon')
       .then((response) => {
-        console.log(response)
+        //console.log(response)
         //CAMINHO DOS POKEMONS
         this.pokemons = response.data.results
+        //console.log(this.pokemons)
       }).catch((err) => {
         console.log(err)
+      })
+    },
+    getColor(){
+      this.$axios.get(this.species + this.index)
+      .then((response) => {
+        //this.color =
+        this.colorUrl = response.data.results
+        console.log(this.colorUrl)
+      }).catch((error) => {
+        console.log(error)
       })
     },
   }
